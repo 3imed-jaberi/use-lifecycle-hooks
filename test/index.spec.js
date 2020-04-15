c859b27c071ede4bbb2de7484124b75949aa2726
3imed-jaberi <imed_jebari@hotmail.fr>
2020-04-15 21:26:28 +0200

    add test for all hooks --jest --enzyme 👨🏼‍🔬 ..

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,36 +1,165 @@
 import React from 'react';
 import { act } from 'react-dom/test-utils';
-import { configure, mount } from 'enzyme';
+import Enzyme, { mount } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
-import { useComponentDidMount } from '../build';
+import { 
+  useComponentWillMount,
+  useComponentDidMount, 
+  useComponentDidUpdate, 
+  useShouldComponentUpdate, 
+  useComponentWillUnmount 
+} from '../build';
 
 
-configure({ adapter: new Adapter() });
+// #######################################################################
+// #################### Config. Enzyme with React 16 #####################
+// #######################################################################
+
+  Enzyme.configure({ adapter: new Adapter() });
 
 
+// #######################################################################
+// ########################## Global Variables ###########################
+// #######################################################################
 
-const ComponentDidMount = () => {
-  // State .. 
-  const [state, setState] = React.useState(' Hello World !');
+  var UnMounting, WillMounting;
 
-  // ComponentDidMount .. 
-  useComponentDidMount(() => {
-    setState('ComponentDidMount ..');
-  });
-
-  return <h1>{state}</h1>;
-}
+  const ComponentWillMountMsg = 'ComponentWillMount ..',
+  ComponentDidMountMsg = 'ComponentDidMount ..',
+  ComponentDidUpdateMsg = 'ComponentDidUpdate ..',
+  ShouldComponentUpdateMsg = 'ShouldComponentUpdate ..',
+  ComponentWillUnmountMsg = 'ComponentWillUnmount ..';
 
 
-describe('My Package ....', () => {
-  test('useComponentDidMount', () => {
-    let component;
+// #######################################################################
+// ########################## Mocks Components ###########################
+// #######################################################################
 
-    act(() => {
-      component = mount(<ComponentDidMount />);
+  function ComponentWillMount () {
+    // State .. 
+    const [state] = React.useState(' Hello World !');
+
+    // ComponentWillMount .. 
+    useComponentWillMount(() => {
+      WillMounting = ComponentWillMountMsg;
     });
 
-    expect(component.text()).toBe('ComponentDidMount ..');
+    return <h1>{state}</h1>;
+  }
+
+  //**********************************************
+
+  function ComponentDidMount () {
+    // State .. 
+    const [state, setState] = React.useState(' Hello World !');
+
+    // ComponentDidMount .. 
+    useComponentDidMount(() => {
+      setState(ComponentDidMountMsg);
+    });
+
+    return <h1>{state}</h1>;
+  }
+
+  //**********************************************
+
+  function ComponentDidUpdate () {
+    // State .. 
+    const [state, setState] = React.useState(' Hello World !');
+
+    // ComponentDidUpdate .. 
+    useComponentDidUpdate(() => {
+      setState(ComponentDidUpdateMsg);
+    });
+    
+    return <h1>{state}</h1>;
+  }
+
+  //**********************************************
+
+  function ShouldComponentUpdate ({ content }) {
+    // State .. 
+    const [state, setState] = React.useState(' Hello World !');
+
+    // ShouldComponentUpdate .. 
+    useShouldComponentUpdate(() => {
+      setState(ShouldComponentUpdateMsg);
+    }, [content]);
+    
+    return <h1>{state}</h1>;
+  }
+
+  //**********************************************
+
+  function ComponentWillUnmount () {
+    // State .. 
+    const [state] = React.useState('Hello World !');
+
+    // ComponentWillUnmount .. 
+    useComponentWillUnmount(() => {
+      // UnMounting here is global var .. 
+      UnMounting = ComponentWillUnmountMsg;
+    });
+    
+    return <h1>{state}</h1>;
+  }
+
+
+// #######################################################################
+// ############################# Test Code ###############################
+// #######################################################################
+
+  describe('use-lifecycle-hooks', () => {
+
+    test('useComponentWillMount', () => {
+      act(() => {
+        mount(<ComponentWillMount />);
+      });
+
+      expect(WillMounting).toBe(ComponentWillMountMsg);
+    });
+
+    test('useComponentDidMount', () => {
+      let component;
+
+      act(() => {
+        component = mount(<ComponentDidMount />);
+      });
+
+      expect(component.text()).toBe(ComponentDidMountMsg);
+    });
+
+    test('useComponentDidUpdate', () => {
+      let component;
+
+      act(() => {
+        component = mount(<ComponentDidUpdate />);
+        component.update();
+      });
+      
+      expect(component.text()).toBe(ComponentDidUpdateMsg);
+    });
+
+    test('useShouldComponentUpdate', () => {
+      let component;
+
+      act(() => {
+        component = mount(<ShouldComponentUpdate />);
+        component.setProps({ content: 'someProps' });
+      });
+      
+      expect(component.text()).toBe(ShouldComponentUpdateMsg);
+    });
+
+    test('useComponentWillUnmount', () => {
+      let component;
+
+      act(() => {
+        component = mount(<ComponentWillUnmount />);
+        component.unmount();
+      });
+      
+      expect(UnMounting).toBe(ComponentWillUnmountMsg);
+    });
   });
-});

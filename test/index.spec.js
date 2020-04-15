c380caaddd5098eaa80335336a876b614aaad006
3imed-jaberi <imed_jebari@hotmail.fr>
2020-04-15 15:52:40 +0200

    add test for useComponentDidMount Hook ⚡️ ..

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,1 +1,36 @@
-// init 🚧 .. 
+import React from 'react';
+import { act } from 'react-dom/test-utils';
+import { configure, mount } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+import { useComponentDidMount } from '../build';
+
+
+configure({ adapter: new Adapter() });
+
+
+
+const ComponentDidMount = () => {
+  // State .. 
+  const [state, setState] = React.useState(' Hello World !');
+
+  // ComponentDidMount .. 
+  useComponentDidMount(() => {
+    setState('ComponentDidMount ..');
+  });
+
+  return <h1>{state}</h1>;
+}
+
+
+describe('My Package ....', () => {
+  test('useComponentDidMount', () => {
+    let component;
+
+    act(() => {
+      component = mount(<ComponentDidMount />);
+    });
+
+    expect(component.text()).toBe('ComponentDidMount ..');
+  });
+});
